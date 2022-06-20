295f63026cbddc67e9be024fdad2da8e822cd948
Anna Khutorianska <annakhutorianska@gmail.com>
2022-06-20 13:56:43 +0300

    add form validation

--- src/components/ButtonRent/ButtonRent.js
+++ src/components/ButtonRent/ButtonRent.js
@@ -1,96 +1,129 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid'
-import Geocode from "react-geocode";
-import { Button, Modal, Input } from 'antd';
+import Geocode from 'react-geocode';
+import { Button, Modal, Input, Form } from 'antd';
 
 import Notification from '../Notification/Notification';
 
 import './ButtonRent.css';
 
 Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
-Geocode.setLanguage("ua");
-Geocode.setRegion("ua");
-Geocode.setLocationType("ROOFTOP");
+Geocode.setLanguage('ua');
+Geocode.setRegion('ua');
+Geocode.setLocationType('ROOFTOP');
 
 function ButtonRent({ setNewAppartment }) {
 	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [appartInfo, setAppartInfo] = useState({});
 	const [isError, setIsError] = useState(false);
+	const [form] = Form.useForm();
 
-	function hangleChange(e) {
-		const {name, value} = e.target;
-		setAppartInfo(prevInfo => ({
-			...prevInfo,
-			[name]: value
-		}))
-	}
-	
 	function toogleModal() {
 		setIsModalVisible((prev) => !prev);
 	}
 
-	function handleOk() {
-		Geocode.fromAddress(appartInfo.address)
+	function onCreate(values) {
+		Geocode.fromAddress(values.address)
 			.then(response => {
-			  const { lat, lng } = response.results[0].geometry.location;
-			  return { lat, lng };
+				const { lat, lng } = response.results[0].geometry.location;
+				return { lat, lng };
 			})
 			.then(coord => {
-				setNewAppartment({ ...appartInfo, coordinates: coord, id:nanoid() })
-				setAppartInfo({})
+				setNewAppartment({ ...values, coordinates: coord, id: nanoid() });
+				form.resetFields();
+				toogleModal();
 			})
-			.catch(error => setIsError(prev => !prev))
+			.catch(() => setIsError(prev => !prev))
 
-		toogleModal();
+	}
+
+	function handleOk() {
+		form
+			.validateFields()
+			.then(values => {
+				onCreate(values);
+			})
+			.catch(info => {
+				console.log('Validate Failed:', info);
+			});
 	}
 
 	return (
 		<>
-			<Button type="primary" onClick={toogleModal} className="button-rent">
+			<Button type='primary' onClick={toogleModal} className='button-rent'>
 				Здати в оренду &#x271A;
 			</Button>
 			<Modal
-				title="Додати оголошення"
+				title='Додати оголошення'
 				visible={isModalVisible}
 				onOk={handleOk}
 				onCancel={toogleModal}
 			>
-				<div className='button-rent-input'>
-					<p className='button-rent-input-label'>Короткий опис</p>
-					<Input
-						name="title"
-						value={appartInfo.title}
-						onChange={hangleChange}
-						placeholder="Input description"
-					/>
-				</div>
-				<div className='button-rent-input'>
-					<p className='button-rent-input-label'>Ціна за добу</p>
-					<Input
-						name="price"
-						value={appartInfo.price}
-						onChange={hangleChange}
-						placeholder="Input price"
-					/>
-				</div>
-				<div className='button-rent-input'>
-					<p className='button-rent-input-label'>Адреса</p>
-					<Input
-						name="address"
-						value={appartInfo.address}
-						onChange={hangleChange}
-						placeholder="Input city, street"
-					/>
-				</div>
-				<div>
-					<p className='button-rent-input-label'>Посилання на зображення</p>
-					<Input
-						name="image"
-						value={appartInfo.image}
-						onChange={hangleChange}
-						placeholder="Input image link"
-					/>
-				</div>
+				<Form
+					form={form}
+					layout='vertical'
+					name='Додати оголошення'
+				>
+					<Form.Item
+						name='title'
+						label='Короткий опис'
+						rules={[
+							{
+								required: true,
+								message: 'Додайте опис',
+							},
+						]}
+					>
+						<Input
+							placeholder='Input description'
+						/>
+					</Form.Item>
+
+					<Form.Item
+						name='price'
+						label='Ціна за добу'
+						rules={[
+							{
+								required: true,
+								message: 'Додайте ціну',
+							},
+						]}
+					>
+						<Input
+							type='number'
+							placeholder='Input price'
+						/>
+					</Form.Item>
+
+					<Form.Item
+						name='address'
+						label='Адреса'
+						rules={[
+							{
+								required: true,
+								message: 'Додайте адресу',
+							},
+						]}
+					>
+						<Input
+							placeholder='Input address'
+						/>
+					</Form.Item>
+
+					<Form.Item
+						name='image'
+						label='image'
+						rules={[
+							{
+								required: true,
+								message: 'Додайте посилання на зображення',
+							},
+						]}
+					>
+						<Input
+							placeholder='Input image'
+						/>
+					</Form.Item>
+				</Form>
 			</Modal>
 			{isError && <Notification setIsError={setIsError} />}
 		</>
